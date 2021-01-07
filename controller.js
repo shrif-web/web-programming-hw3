2a98ba220f81fda0ebce465f9853ba5cd66bd49b
pourya-momtaz <pouryamz19@gmail.com>
2021-01-07 17:15:43 +0330

    button positioning changed

--- controller.js
+++ controller.js
@@ -1,9 +1,11 @@
 function showInfo() {
     document.getElementById("posts").style.display = "none";
-    document.getElementById("info").style.display = "block";
+    document.getElementById("addButton").style.display = "none";
+    document.getElementById("info").style.display = "inline-block";
   }
 
   function showPersonalPosts() {
     document.getElementById("info").style.display = "none";
-    document.getElementById("posts").style.display = "block";
+    document.getElementById("posts").style.display = "inline-block";
+    document.getElementById("addButton").style.display = "inline-block";
   }
