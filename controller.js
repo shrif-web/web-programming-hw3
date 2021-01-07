fc084f7444fdc7a9ae143970cc178556b790d627
Mohammadjafari80 <mamad.jafari91@gmail.com>
2021-01-07 17:13:23 +0330

    fix body width bug

--- controller.js
+++ controller.js
@@ -1,9 +1,12 @@
 function showInfo() {
     document.getElementById("posts").style.display = "none";
     document.getElementById("info").style.display = "block";
+    document.getElementById("main-section").style.justifyContent = "center";
   }
 
   function showPersonalPosts() {
     document.getElementById("info").style.display = "none";
     document.getElementById("posts").style.display = "block";
+    document.getElementById("main-section").style.justifyContent = "right";
+
   }
