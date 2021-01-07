7bcb77c230fcce6b4b47d594bf075303b96ff934
pourya-momtaz <pouryamz19@gmail.com>
2021-01-07 17:51:14 +0330

    functions moved to controller

--- public/controller.js
+++ public/controller.js
@@ -9,3 +9,64 @@ function showInfo() {
     document.getElementById("posts").style.display = "inline-block";
     document.getElementById("addButton").style.display = "inline-block";
   }
+
+  
+function signup() {
+    let details = {
+        "email": document.getElementById("email-signup").value,
+        "password": document.getElementById("pass-signup").value,
+    };
+    if (details['password'] != document.getElementById("repass-signup").value) {
+        alert('Password not match!')
+    } else if (!document.getElementById("accept").checked) {
+        alert('You should accept user agrement.');
+    } else {
+        let formBody = [];
+        for (var property in details) {
+            formBody.push(encodeURIComponent(property) + "=" + encodeURIComponent(details[property]));
+        }
+        let http = new XMLHttpRequest();
+        let url = 'api/signup';
+        let params = formBody.join("&");
+        http.open('POST', url, true);
+        http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        http.send(params);
+        http.onreadystatechange = function () {
+            if (http.readyState == 4) {
+                if (http.status != 201) {
+                    alert(JSON.parse(http.responseText)['message']);
+                } else {
+                    window.location.pathname = '/index.html'
+                }
+            }
+        }
+    }
+}
+
+
+function signin() {
+    let details = {
+        "email": document.getElementById("email-signin").value,
+        "password": document.getElementById("pass-signin").value
+    };
+    let formBody = [];
+    for (var property in details) {
+        formBody.push(encodeURIComponent(property) + "=" + encodeURIComponent(details[property]));
+    }
+    let http = new XMLHttpRequest();
+    let url = 'api/signin';
+    let params = formBody.join("&");
+    http.open('POST', url, true);
+    http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    http.send(params);
+    http.onreadystatechange = function () {
+        if (http.readyState == 4) {
+            if (http.status != 201) {
+                alert(JSON.parse(http.responseText)['message']);
+            } else {
+                window.location.pathname = '/index.html'
+            }
+        }
+    }
+}
+                                
